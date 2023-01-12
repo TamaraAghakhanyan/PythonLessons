9ce1fd3c69c4ba77670835e5ed003cf1b0cf60ce
TomaAghakhanyan <t.aghakhanyan@gmail.com>
2023-01-13 01:38:19 +0400

    Conditionals; Loops and Exceptions.py

--- Conditionals; Loops and Exceptions.py
+++ Conditionals; Loops and Exceptions.py
@@ -122,7 +122,7 @@ for i in range(10):
 
 #Try/Except/Else/Finally
 
-for i in range(5):
+'''for i in range(5):
     try:
         print(i / 0)
     except ZeroDivisionError as e:
@@ -172,8 +172,33 @@ try:
 except NameError:
     print("Name Error!")
 finally:
-    print("I don't care, I'm getting printed either way!")
+    print("I don't care, I'm getting printed either way!"'''
 
+'''x = 10
+if x < 10:
+	for i in range(1, 5):
+		print(x * i)
+elif x > 10:
+	j = 1
+	while j < 5:
+		print(x * j)
+		j = j + 1
+else:
+	print(x ** 10)'''
+
+x = "Hello Python"
+
+if x.startswith("H") or len(x) > 12:
+
+	print('/'.join(x[:7]))
+
+elif x[-1] == "n" and len(x.split('o')) >= 3:
+
+    print(x.lower()[4:])
+
+else:
+
+	print((x + ' ') * 3 + "!")
 
 
 
