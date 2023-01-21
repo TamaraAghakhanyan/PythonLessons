204773f93c49c96c8d94a64455d91ff4e31e5cd0
TomaAghakhanyan <t.aghakhanyan@gmail.com>
2023-01-22 00:45:22 +0400

    Pyhon basic exercises part1

--- Loops exercises.py
+++ Loops exercises.py
@@ -34,14 +34,85 @@ while x <= 10:
     elif i % 3 == 0:
         print('Fizz')
     else:
-        print(i)'''
+        print(i)
 
 
 x = 6
 x += 3
 print(x)
 import random
-print(dir(random))
+print(dir(random))'''
+
+
+'''Write a program to count the total number of digits in a number using a while loop.
+For example, the number is 75869, so the output should be 5.
+
+n = int(input("Enter number: "))
+
+count = 0
+while n > 0:
+    count = count + 1
+    n = n // 10
+print("The number of digits in the number are:", count)'''
+
+
+#Exercise 8. print list in reversed order
+'''list = [10, 20, 30, 40, 50]
+
+a = list[::-1]
+
+for i in a:
+    print(i)'''
+
+
+#Exercise 9.Display numbers from -10 to -1 using for loop
+
+'''start = -10
+end = 0
+
+for i in range(start,end,1):
+    print(i)'''
+
+#Exercise 10: Use else block to display a message “Done” after successful execution of for loop
+
+'''n = int(input("Enter Number: "))
+
+for i in range(n):
+    if i == n - 1:
+       print(i)
+       print("Done!")
+    else:
+        print(i)'''
+
+
+#Exercise 11: Write a program to display all prime numbers within a range
+
+
+# find factor of number
+'''num = int(input('Enter number: '))
+
+print('The factors of', num, 'are:')
+for i in range(1, num+1):
+    if(num % i) == 0:
+        print(i, end=' ')'''
+
+a = int(input("Enter 1st Number: "))
+n = int(input("Enter the last Number: "))
+
+print("Prime numbers between", a, "and", n, "are:")
+for num in range(a, n + 1):
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+
+
+
+
+
+
 
 
 
