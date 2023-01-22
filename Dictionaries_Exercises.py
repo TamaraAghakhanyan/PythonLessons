5198d9dc9cc4c6d842ace08f8881a178825e0e71
TomaAghakhanyan <t.aghakhanyan@gmail.com>
2023-01-22 21:29:41 +0400

    Dictionaries.py

---
+++ Dictionaries_Exercises.py
@@ -0,0 +1,18 @@
+# 1. Write a Python script to sort (ascending and descending) a dictionary by value.
+# to solve after lambda
+
+# 2. Write a Python script to add a key to a dictionary.
+
+dict = {0: 10, 1: 20}
+dict[2] = 30
+print(dict)
+
+# 3. Write a Python script to concatenate following dictionaries to create a new one.
+# Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+# Sample Dictionary :
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+
+
