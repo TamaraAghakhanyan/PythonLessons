11e7c291570bae555fa02131798f8d8cc0c3bf8b
TomaAghakhanyan <t.aghakhanyan@gmail.com>
2023-01-22 21:55:38 +0400

    Dictionaries.py

--- Dictionaries_Exercises.py
+++ Dictionaries_Exercises.py
@@ -15,4 +15,21 @@ dic1={1:10, 2:20}
 dic2={3:30, 4:40}
 dic3={5:50,6:60}
 
+# Solution1
+dic1.update(dic2)
+print(dic1)
+dic1.update(dic3)
+print(dic1)
 
+# Solution2
+dic4 = {}
+
+for d in (dic1, dic2, dic3):
+    dic4.update(d)
+print(dic4)
+
+
+# 4. Write a Python script to check whether a given key already exists in a dictionary.
+dic1={1:10, 2:20}
+
+print(1 in dic1)
