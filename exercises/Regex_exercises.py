0fc9f7880d8f349fce4d9a7cf2b99d4648ed2c25
TomaAghakhanyan <t.aghakhanyan@gmail.com>
2023-06-27 21:32:38 +0400

    Merge remote-tracking branch 'origin/homeworks' into homeworks

---
+++ exercises/Regex_exercises.py
@@ -0,0 +1,5 @@
+'''1. Write a Python program to check that a string contains only a certain
+      set of characters (in this case a-z, A-Z and 0-9)'''
+# a = int(input(str()))
+# import re
+# result = re.search("[a-z] [A-Z][0-9]")
