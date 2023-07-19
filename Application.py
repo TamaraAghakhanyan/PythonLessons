875d55af23d3f1ed1364b1c5ac932384f7780b93
TomaAghakhanyan <t.aghakhanyan@gmail.com>
2023-07-19 15:13:29 +0400

    database

--- Application.py
+++ Application.py
@@ -29,3 +29,16 @@ except psycopg2.Error as e:
     print(f"Error connecting to the database: {e}")
 
 cursor = connection.cursor()
+
+# insert all the data into the database using cursor/ insert into method:
+try:
+    for i in records:
+        cursor.execute("insert into mystaff.employees (id,first_name,last_name,department,phone,address,salary) values (%s,%s,%s,%s,%s,%s,%s);",(i[0],i[1],i[2],i[3],i[4],i[5],i[6]))
+
+except psycopg2.Error as e:
+    print(f"Error connecting to the database: {e}")
+
+else:
+    print("Records inserted successfully!")
+
+connection.commit()
