3fbcaf696e1525fff2c53b98bcd9e11147164c78
TomaAghakhanyan <t.aghakhanyan@gmail.com>
2023-10-13 15:36:38 +0400

    exercise solution

--- exercises/Exercises.py
+++ exercises/Exercises.py
@@ -1012,7 +1012,6 @@ Write a program to print the following start pattern using the for loop
 '''
 # row = 5
 #
-#
 # for i in range(1,row + 1, 1):
 #     for j in range(1, i + 1):
 #         print("*", end= " ")
