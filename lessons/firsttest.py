4f00cad620e82fcd548ea60ebcdc78e1a07f4a8c
TomaAghakhanyan <t.aghakhanyan@gmail.com>
2023-12-11 17:57:44 +0400

    pytest

--- lessons/firsttest.py
+++ lessons/firsttest.py
@@ -1,8 +1,17 @@
 import pytest
 
-def my_func(x, y, z):
-    return x + y + z
+# def my_func(x, y, z):
+#     return x + y + z
+#
+# def test_result1():
+#     assert (1, 2, 3) == 5
 
-def test_result1():
-    assert (1, 2, 3) == 5
+def my_exeption():
+    div = 10 / 0
+    return div
+
+def test_result2():
+    with pytest.raises(ZeroDivisionError):
+        my_exeption()
+
 
